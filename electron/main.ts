1dc13f1fb361c5fababbb36f0be09695610b6ef2
Fernando Maclen <fmaclen@gmail.com>
2022-07-23 13:07:56 -0400

    Add Playwright

--- electron/main.ts
+++ electron/main.ts
@@ -3,7 +3,7 @@ import { app } from "electron";
 import { stopServer } from "./server";
 import setTray from "./tray";
 
-process.platform === "darwin" && app.dock.hide(); // Hide dock on macOS
+process.platform === "darwin" && app.dock.hide(); // Hide dock icon on macOS
 
 app.whenReady().then(() => {
   setTray();
