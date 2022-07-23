fdd72405e3b9d9959ca2ae9c45d9d49715ff407c
Fernando Maclen <fmaclen@gmail.com>
2022-07-22 23:12:36 -0400

    Add "Open vault" option to tray context menu

--- electron/server.ts
+++ electron/server.ts
@@ -7,20 +7,26 @@ const serverPort = app.isPackaged ? "42069" : "3000";
 export const serverUrl = `http://localhost:${serverPort}`;
 
 let serverPid: number | undefined;
-export const startServer = async () => {
+export const startServer = async (vaultPath: string) => {
   const HOST = "127.0.0.1";
-  const databaseUrl = "file:./Canutin.vault"; // FIXME: this should be loaded from an Electron dialog — https://www.electronjs.org/docs/latest/api/dialog
 
   // Svelte's build with `@adapter-node`
   const serverModulePath = app.isPackaged
     ? path.join(process.resourcesPath, `sveltekit/index.js`)
-    : `${path.resolve(__dirname, "../../sveltekit/build/index.js")}`;
+    : path.resolve(__dirname, "../../sveltekit/build/index.js");
 
   const { pid } = fork(serverModulePath, {
-    env: { ...process.env, HOST, PORT: serverPort, DATABASE_URL: databaseUrl },
+    env: {
+      ...process.env,
+      HOST,
+      PORT: serverPort,
+      DATABASE_URL: `file:${vaultPath}`,
+    },
   });
 
   serverPid = pid; // Set process id so we can kill it later
+
+  app.isPackaged && console.log(`\n-> Server started at ${serverUrl}\n`);
 };
 
 export const stopServer = () => {
--- electron/tray.ts
+++ electron/tray.ts
@@ -1,69 +1,147 @@
 import path from "path";
-import { app, Tray, Menu, shell } from "electron";
+import {
+  app,
+  Tray,
+  Menu,
+  shell,
+  dialog,
+  MenuItemConstructorOptions,
+  MenuItem,
+} from "electron";
 
 import { serverUrl, startServer, stopServer } from "./server";
 
-const SERVER_STATUS_NEGATIVE = "tray-server-status-negative";
-const SERVER_STATUS_POSITIVE = "tray-server-status-positive";
-const SERVER_START = "tray-server-start";
-const SERVER_STOP = "tray-server-stop";
-const OPEN_CANUTIN = "tray-open-canutin";
+const MENU_SERVER_STATUS = "menu-server-status";
+const MENU_SERVER_TOGGLE = "menu-server-toggle";
+const MENU_OPEN_IN_BROWSER = "menu-open-in-browser";
+const MENU_VAULT_PATH = "menu-vault-path";
+const MENU_VAULT_OPEN = "menu-vault-open";
 
 let isServerRunning: boolean = false;
+let vaultPath: string | undefined;
 
 const imgPath = (fileName: string) =>
   app.isPackaged
     ? path.join(process.resourcesPath, `assets/${fileName}.png`)
     : `./resources/assets/${fileName}.png`;
 
-// Default tray menu template
-const trayTemplate = Menu.buildFromTemplate([
-  {
-    label: "Canutin is not running",
+const openBrowser = () => {
+  shell.openExternal(serverUrl);
+};
 
-    icon: imgPath("status-negative"),
-    id: SERVER_STATUS_NEGATIVE,
-    enabled: false,
-  },
+const menuServerStatus = {
+  label: "Canutin is not running",
+  icon: imgPath("status-negative"),
+  id: MENU_SERVER_STATUS,
+  enabled: false,
+} as MenuItemConstructorOptions;
+
+const menuServerToggle = {
+  label: "Start Canutin",
+  click: () => toggleServer(),
+  id: MENU_SERVER_TOGGLE,
+  visible: false,
+} as MenuItemConstructorOptions;
+
+const menuOpenInBrowser = {
+  label: "Open in browser",
+  id: MENU_OPEN_IN_BROWSER,
+  accelerator: process.platform === "darwin" ? "Command+O" : "Ctrl+0",
+  visible: false,
+  click: () => openBrowser(),
+} as MenuItemConstructorOptions;
+
+const menuVaultPath = {
+  label: "No vault chosen",
+  id: MENU_VAULT_PATH,
+  enabled: false,
+} as MenuItemConstructorOptions;
+
+const menuOpenVault = {
+  label: "Open vault...",
+  id: MENU_VAULT_OPEN,
+  click: () => openVault(),
+} as MenuItemConstructorOptions;
+
+const menuPresistentOptions = [
   {
-    label: "Canutin is running",
-    icon: imgPath("status-positive"),
-    id: SERVER_STATUS_POSITIVE,
-    enabled: false,
-    visible: false,
+    label: "About",
+    click: () => app.showAboutPanel(),
   },
-  { type: "separator" },
-  {
-    label: "Run Canutin",
-    id: SERVER_START,
-    click: () => toggleServer(),
-  },
-  {
-    label: "Stop Canutin",
-    id: SERVER_STOP,
-    visible: false,
-    click: () => toggleServer(),
-  },
-  { type: "separator" },
-  {
-    label: "Open Canutin",
-    id: OPEN_CANUTIN,
-    visible: false,
-    click: () => openBrowser(),
-  },
-  { label: "About", click: () => app.showAboutPanel() },
   {
     label: "Quit",
     accelerator: process.platform === "darwin" ? "Command+Q" : "Alt+F4",
     click: () => app.quit(),
   },
-]);
+] as MenuItemConstructorOptions[];
+
+const menuSeparator = { type: "separator" } as MenuItem;
+
+let currentTemplate = [
+  menuServerStatus,
+  menuSeparator,
+  menuServerToggle,
+  menuOpenInBrowser,
+  menuSeparator,
+  menuVaultPath,
+  menuOpenVault,
+  menuSeparator,
+  ...menuPresistentOptions,
+] as MenuItemConstructorOptions[];
+
+const updateContextMenu = () => {
+  tray.setContextMenu(Menu.buildFromTemplate(currentTemplate));
+};
+
+const openVault = () => {
+  const dialogPaths = dialog.showOpenDialogSync({
+    properties: ["openFile"],
+    filters: [{ name: "Canutin Vault", extensions: ["vault"] }],
+  });
+
+  if (!dialogPaths) return;
+  vaultPath = dialogPaths[0];
+  menuVaultPath.label = vaultPath;
+
+  if (isServerRunning) {
+    // Restarting the server by toggling it off and then on.
+    toggleServer();
+    toggleServer();
+  } else {
+    // Start server
+    toggleServer();
+  }
+
+  updateContextMenu();
+};
+
+const toggleServer = () => {
+  if (isServerRunning) {
+    // Stop the server
+    menuServerToggle.label = "Start Canutin";
+    menuServerStatus.label = "Canutin is not running";
+    menuServerStatus.icon = imgPath("status-negative");
+    updateContextMenu();
+    stopServer();
+    tray.setImage(imgPath("canutin-tray-idle"));
+  } else if (vaultPath) {
+    // Start the server
+    menuServerToggle.visible = true;
+    menuServerToggle.label = "Stop Canutin";
+    menuServerStatus.label = "Canutin is running";
+    menuServerStatus.icon = imgPath("status-positive");
+    updateContextMenu();
+    startServer(vaultPath);
+    tray.setImage(imgPath("canutin-tray-active"));
+  }
+  isServerRunning = !isServerRunning;
+};
 
 let tray: Tray;
-export const setTray = () => {
+const setTray = () => {
   tray = new Tray(imgPath("canutin-tray-idle"));
   tray.setToolTip("Canutin");
-  tray.setContextMenu(trayTemplate);
+  tray.setContextMenu(Menu.buildFromTemplate(currentTemplate));
 
   toggleServer(); // Server runs when the app starts
 
@@ -71,52 +149,7 @@ export const setTray = () => {
   // To prevent opening the browser before the server is ready we wait 500ms.
   // Server boot up time is likely to vary so ideally we would "ping"
   // the server until it's ready and only then open the user's browser.
-  //
-  // Open the user's browser to the server's URL
   app.isPackaged && setTimeout(() => openBrowser(), 500);
-};
-
-const openBrowser = () => {
-  shell.openExternal(serverUrl);
-};
-
-const serverStatusNegative = trayTemplate.getMenuItemById(
-  SERVER_STATUS_NEGATIVE
-);
-const serverStatusPositive = trayTemplate.getMenuItemById(
-  SERVER_STATUS_POSITIVE
-);
-const serverStart = trayTemplate.getMenuItemById(SERVER_START);
-const serverStop = trayTemplate.getMenuItemById(SERVER_STOP);
-const openCanutin = trayTemplate.getMenuItemById(OPEN_CANUTIN);
-
-const toggleServer = () => {
-  if (
-    serverStatusNegative &&
-    serverStatusPositive &&
-    serverStart &&
-    serverStop &&
-    openCanutin
-  ) {
-    if (isServerRunning) {
-      stopServer();
-      tray.setImage(imgPath("canutin-tray-idle"));
-      serverStatusNegative.visible = true;
-      serverStatusPositive.visible = false;
-      serverStart.visible = true;
-      serverStop.visible = false;
-      openCanutin.visible = false;
-    } else {
-      startServer();
-      tray.setImage(imgPath("canutin-tray-active"));
-      serverStatusNegative.visible = false;
-      serverStatusPositive.visible = true;
-      serverStart.visible = false;
-      serverStop.visible = true;
-      openCanutin.visible = true;
-    }
-    isServerRunning = !isServerRunning;
-  }
 };
 
 export default setTray;
