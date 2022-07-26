67343aa244834019331b0dde9de36ffb503ef363
Fernando Maclen <fmaclen@gmail.com>
2022-07-25 22:55:44 -0400

    Add tests for Vault class

---
+++ electron/__tests__/vault.spec.ts
@@ -0,0 +1,22 @@
+import Store from "electron-store";
+
+import Vault from "../vault";
+
+describe("Vault", () => {
+  let vault: Vault;
+
+  beforeEach(() => {
+    vault = new Vault();
+    vault.saveToUserSettings(null);
+  });
+
+  test("vault path can't be read from user settings", () => {
+    expect(vault["userSettings"]).toBeInstanceOf(Store);
+    expect(vault.path).toBe(null);
+  });
+
+  test("vault path can be saved and read from user settings", () => {
+    vault.saveToUserSettings("/fake/path/to/Canutin.vault");
+    expect(vault.path).toBe("/fake/path/to/Canutin.vault");
+  });
+});
