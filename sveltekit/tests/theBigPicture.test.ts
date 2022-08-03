5387b068884d30c300cf3258a27bd4a38291e20c
Fernando Maclen <fmaclen@gmail.com>
2022-08-03 15:48:14 -0700

    Add Big picture tests (#13)
    * Add Big picture tests
    
    * Add test comments

--- sveltekit/tests/theBigPicture.test.ts
+++ sveltekit/tests/theBigPicture.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test';
-import { checkVaultIsDev, wipeVault } from './fixtures/helpers.js';
+import { checkVaultIsDev, importCanutinFile, wipeVault } from './fixtures/helpers.js';
 
-test.skip('Balance sheet', () => {
+test.describe('Balance sheet', () => {
 	test.beforeAll(() => {
 		checkVaultIsDev();
 	});
@@ -10,7 +10,35 @@ test.skip('Balance sheet', () => {
 		await wipeVault();
 	});
 
-	test('Summary totals are calculated correctly', async ({ page }) => {
-		// TODO
+	test('Summary totals are calculated correctly', async ({ page, baseURL }) => {
+		await page.goto('/');
+		await expect(page.locator('h1', { hasText: 'The big picture' })).toBeVisible();
+
+		// Check that the balanceGroups are in the correct order
+		const balanceGroups = page.locator('.card');
+		expect(await balanceGroups.count()).toBe(5);
+		expect(await balanceGroups.nth(0).textContent()).toMatch('Net worth');
+		expect(await balanceGroups.nth(1).textContent()).toMatch('Cash');
+		expect(await balanceGroups.nth(2).textContent()).toMatch('Debt');
+		expect(await balanceGroups.nth(3).textContent()).toMatch('Investments');
+		expect(await balanceGroups.nth(4).textContent()).toMatch('Other assets');
+
+		// Check that the balacneGroups have the correct amounts
+		expect(await page.locator('.card', { hasText: 'Net worth' }).textContent()).toMatch('$0');
+		expect(await page.locator('.card', { hasText: 'Cash' }).textContent()).toMatch('$0');
+		expect(await page.locator('.card', { hasText: 'Debt' }).textContent()).toMatch('$0');
+		expect(await page.locator('.card', { hasText: 'Investments' }).textContent()).toMatch('$0');
+		expect(await page.locator('.card', { hasText: 'Other assets' }).textContent()).toMatch('$0');
+
+		// Check that the balanceGroups have the correct amounts after importing data
+		await importCanutinFile(baseURL!, 'minimum-data');
+		await page.reload();
+		expect(await page.locator('.card', { hasText: 'Net worth' }).textContent()).toMatch('$7,571');
+		expect(await page.locator('.card', { hasText: 'Cash' }).textContent()).toMatch('$0');
+		expect(await page.locator('.card', { hasText: 'Debt' }).textContent()).toMatch('$0');
+		expect(await page.locator('.card', { hasText: 'Investments' }).textContent()).toMatch('$0');
+		expect(await page.locator('.card', { hasText: 'Other assets' }).textContent()).toMatch(
+			'$7,571'
+		);
 	});
 });
