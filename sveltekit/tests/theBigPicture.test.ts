c40452e3861b6898ef2b73d6844a8afbff6f3abb
Fernando Maclen <fmaclen@gmail.com>
2022-08-03 18:44:35 -0400

    Add test comments

--- sveltekit/tests/theBigPicture.test.ts
+++ sveltekit/tests/theBigPicture.test.ts
@@ -13,12 +13,8 @@ test.describe('Balance sheet', () => {
 	test('Summary totals are calculated correctly', async ({ page, baseURL }) => {
 		await page.goto('/');
 		await expect(page.locator('h1', { hasText: 'The big picture' })).toBeVisible();
-		expect(await page.locator('.card', { hasText: 'Net worth' }).textContent()).toMatch('$0');
-		expect(await page.locator('.card', { hasText: 'Cash' }).textContent()).toMatch('$0');
-		expect(await page.locator('.card', { hasText: 'Debt' }).textContent()).toMatch('$0');
-		expect(await page.locator('.card', { hasText: 'Investments' }).textContent()).toMatch('$0');
-		expect(await page.locator('.card', { hasText: 'Other assets' }).textContent()).toMatch('$0');
 
+		// Check that the balanceGroups are in the correct order
 		const balanceGroups = page.locator('.card');
 		expect(await balanceGroups.count()).toBe(5);
 		expect(await balanceGroups.nth(0).textContent()).toMatch('Net worth');
@@ -27,6 +23,14 @@ test.describe('Balance sheet', () => {
 		expect(await balanceGroups.nth(3).textContent()).toMatch('Investments');
 		expect(await balanceGroups.nth(4).textContent()).toMatch('Other assets');
 
+		// Check that the balacneGroups have the correct amounts
+		expect(await page.locator('.card', { hasText: 'Net worth' }).textContent()).toMatch('$0');
+		expect(await page.locator('.card', { hasText: 'Cash' }).textContent()).toMatch('$0');
+		expect(await page.locator('.card', { hasText: 'Debt' }).textContent()).toMatch('$0');
+		expect(await page.locator('.card', { hasText: 'Investments' }).textContent()).toMatch('$0');
+		expect(await page.locator('.card', { hasText: 'Other assets' }).textContent()).toMatch('$0');
+
+		// Check that the balanceGroups have the correct amounts after importing data
 		await importCanutinFile(baseURL!, 'minimum-data');
 		await page.reload();
 		expect(await page.locator('.card', { hasText: 'Net worth' }).textContent()).toMatch('$7,571');
