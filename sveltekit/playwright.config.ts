72145ba06160fae4193209fb46df60010b57088c
Fernando Maclen <fmaclen@gmail.com>
2022-08-03 13:47:15 -0700

    Balance sheet tests (#11)
    * Update README.md
    
    * Add placeholder balanceGroups when they don't have balances
    
    * Add Balance sheet tests
    
    * Add config comments
    
    * Add test comments

--- sveltekit/playwright.config.ts
+++ sveltekit/playwright.config.ts
@@ -8,7 +8,10 @@ const config: PlaywrightTestConfig = {
 	use: {
 		headless: true,
 		trace: 'off'
-	}
+		// trace: 'retain-on-failure' // uncomment to see use TraceViewer when a test fails
+	},
+	// Can't have more than 1 worker because the tests read/write to the same DB at the same time
+	workers: 1
 };
 
 export default config;
