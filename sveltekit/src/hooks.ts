1ffb6c7921ff185e3897bd186eef6d8fccf2ec95
Fernando Maclen <fmaclen@gmail.com>
2022-08-23 11:40:29 -0400

    #30 - Force redirect to use `http:` in production

--- sveltekit/src/hooks.ts
+++ sveltekit/src/hooks.ts
@@ -5,8 +5,13 @@ export const handle: Handle = async ({ event, resolve }) => {
 	// Every time the vault is switched we need to check if it's migrated and seeded
 	if (env.ELECTRON_SWITCHED_VAULT === 'true') {
 		// Don't redirect if the current path is `/vault`
-		if (!event.url.pathname.startsWith('/vault'))
-			return Response.redirect(`${event.url.origin}/vault`, 307);
+		if (!event.url.pathname.startsWith('/vault')) {
+			// Force redirect to not use `http:` in production because it'a always `localhost`
+			const url = new URL(event.url.origin);
+			url.protocol = 'http:';
+
+			return Response.redirect(`${url.origin}/vault`, 307);
+		}
 	}
 
 	// No changes to the vault, continue as normal
