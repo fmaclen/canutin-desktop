21eb03a3bc197369cb58be9406da7459721cff3f
Fernando Maclen <fmaclen@gmail.com>
2022-08-26 21:50:53 -0400

    #37 — Customize error pages (#53)
    * #37 - Add custom layout for error pages
    
    * #37 - Add test and comments
    
    * #37 - Update test assertion
    
    Fixes #37

--- sveltekit/src/hooks.ts
+++ sveltekit/src/hooks.ts
@@ -1,20 +1,27 @@
 import type { Handle } from '@sveltejs/kit';
 import { env } from '$env/dynamic/private';
+import { dev } from '$app/env';
 
 export const handle: Handle = async ({ event, resolve }) => {
+	// Simulate an internal server error by visiting `/500` in dev
+	if (dev && event.url.pathname.startsWith('/500'))
+		throw new Error(
+			'This error is intentional and should be referenced by a test. If you see this in production god help us all!'
+		);
+
 	// Every time the vault is switched we need to check if it's migrated and seeded.
 	// The check is performed by the `/vault` route so we need to redirect any request
 	// to that path when the flag `ELECTRON_SWITCHED_VAULT=true` is set.
-
-	const skipRedirectPaths = [
+	//
+	const skipPathNames = [
 		'/vault', // Don't redirect when the path is already `/vault`
 		'/devTools' // We use `/devTools` in tests to bypass the `/vault` logic
 	];
-	const shouldRedirect = !skipRedirectPaths.some((pathName) =>
-		event.url.pathname.includes(pathName)
-	);
+	const shouldRedirect =
+		env.ELECTRON_SWITCHED_VAULT === 'true' &&
+		!skipPathNames.some((pathName) => event.url.pathname.includes(pathName));
 
-	if (shouldRedirect && env.ELECTRON_SWITCHED_VAULT === 'true') {
+	if (shouldRedirect) {
 		// Force redirect to not use `http:` in production because it'a always `localhost`
 		const url = new URL(event.url.origin);
 		url.protocol = 'http:';
---
+++ sveltekit/src/routes/+error.svelte
@@ -0,0 +1,34 @@
+<script>
+	import { page } from '$app/stores';
+	import { dev } from '$app/env';
+	import ScrollView from '$lib/components/ScrollView.svelte';
+	import Section from '$lib/components/Section.svelte';
+	import Notice from '$lib/components/Notice.svelte';
+	import Code from '$lib/components/Code.svelte';
+
+	const title = `${$page.status === 404 ? 'Not found' : 'Something went wrong'}`;
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
+
+<ScrollView {title} isFullscreen={true}>
+	<Section title={`Error ${$page.status.toString()}`}>
+		<div slot="CONTENT">
+			<Notice isError={$page.status !== 404}>
+				{$page.status === 404
+					? "No content found. Perhaps there's a typo in the address or followed a broken link"
+					: "An error ocurred and whatever was happening likely didn't finish succesfully"}
+			</Notice>
+
+			{#if dev}
+				<p class="errorMessage">
+					<Code>
+						{$page.error.message}
+					</Code>
+				</p>
+			{/if}
+		</div>
+	</Section>
+</ScrollView>
