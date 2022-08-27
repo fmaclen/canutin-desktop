fe4b1b0249452cb665c7784964db4534a874b04e
Fernando Maclen <fmaclen@gmail.com>
2022-08-27 16:42:00 -0400

    #54 - Remove appIsBusy animation

--- electron/tray-menu.ts
+++ electron/tray-menu.ts
@@ -28,7 +28,6 @@ class TrayMenu {
   static readonly ICON_STATUS_NEGATIVE = "status-negative";
 
   private isServerRunning: boolean;
-  private isAppBusy: boolean;
   private isAppPackaged: boolean;
   private isMacOs: boolean;
 
@@ -49,9 +48,9 @@ class TrayMenu {
   constructor(vault: Vault) {
     this.vault = vault;
     this.isServerRunning = false;
-    this.isAppBusy = true;
     this.isAppPackaged = app.isPackaged || false;
     this.isMacOs = process.platform === "darwin";
+    this.trayIcon = TrayMenu.ICON_TRAY_IDLE;
 
     // Menu items (not in order)
     this.menuSeparator = { type: "separator" };
@@ -122,9 +121,6 @@ class TrayMenu {
     this.tray.setToolTip("Canutin");
     this.tray.setContextMenu(Menu.buildFromTemplate(this.menuCurrentTemplate));
 
-    // Intilialize the tray icon animation
-    this.setTrayIconBusy();
-
     // Update the tray menu if the OS color theme changes
     nativeTheme.on("updated", () => {
       this.setTrayIcon(this.trayIcon);
@@ -157,7 +153,6 @@ class TrayMenu {
   };
 
   private toggleServer = () => {
-    this.isAppBusy = true;
     const vaultPath = this.vault?.path;
 
     if (!this.server && vaultPath) this.server = new Server(vaultPath);
@@ -171,15 +166,8 @@ class TrayMenu {
         TrayMenu.ICON_STATUS_NEGATIVE
       );
       this.menuOpenInBrowser.visible = false;
+      this.setTrayIcon(TrayMenu.ICON_TRAY_IDLE);
       this.updateTray();
-
-      // FIXME: remove setTimeout and instead check if the server is still listening to requests
-      // REF: https://github.com/Canutin/desktop-2/issues/8
-      setTimeout(() => {
-        // Runs one loop of the tray icon animation before switching to `ICON_TRAY_IDLE`
-        this.isAppBusy = false;
-        this.setTrayIcon(TrayMenu.ICON_TRAY_IDLE);
-      }, TrayMenu.ICON_TRAY_LOOP_DURATION);
     } else if (this.server) {
       // Start the server
       this.server.start(vaultPath);
@@ -190,15 +178,8 @@ class TrayMenu {
         TrayMenu.ICON_STATUS_POSITIVE
       );
       this.menuOpenInBrowser.visible = true;
+      this.setTrayIcon(TrayMenu.ICON_TRAY_ACTIVE);
       this.updateTray();
-
-      // FIXME: remove setTimeout and instead check if the server is still listening to requests
-      // REF: https://github.com/Canutin/desktop-2/issues/8
-      setTimeout(() => {
-        // Runs one loop of the tray icon animation before switching to `ICON_TRAY_ACTIVE`
-        this.isAppBusy = false;
-        this.setTrayIcon(TrayMenu.ICON_TRAY_ACTIVE);
-      }, TrayMenu.ICON_TRAY_LOOP_DURATION);
     }
 
     // FIXME:
@@ -242,34 +223,6 @@ class TrayMenu {
   private setTrayIcon = (icon: string) => {
     this.trayIcon = icon;
     this.tray.setImage(this.getImagePath(icon));
-  };
-
-  private setTrayIconBusy = () => {
-    // Animates the tray icon by cycling through the frames below
-    enum TrayBusyIconFrames {
-      FRAME_1 = "canutin-tray-busy-frame1",
-      FRAME_2 = "canutin-tray-busy-frame2",
-      FRAME_3 = "canutin-tray-busy-frame3",
-    }
-
-    const toggleAnimation = () => {
-      if (!this.isAppBusy) return;
-
-      setTimeout(() => {
-        this.isAppBusy && this.setTrayIcon(TrayBusyIconFrames.FRAME_2);
-      }, TrayMenu.ICON_TRAY_LOOP_DURATION * 0);
-      setTimeout(() => {
-        this.isAppBusy && this.setTrayIcon(TrayBusyIconFrames.FRAME_3);
-      }, TrayMenu.ICON_TRAY_LOOP_DURATION * 0.25);
-      setTimeout(() => {
-        this.isAppBusy && this.setTrayIcon(TrayBusyIconFrames.FRAME_2);
-      }, TrayMenu.ICON_TRAY_LOOP_DURATION * 0.5);
-      setTimeout(() => {
-        this.isAppBusy && this.setTrayIcon(TrayBusyIconFrames.FRAME_1);
-      }, TrayMenu.ICON_TRAY_LOOP_DURATION * 0.75);
-    };
-
-    setInterval(toggleAnimation, TrayMenu.ICON_TRAY_LOOP_DURATION);
   };
 }
 
