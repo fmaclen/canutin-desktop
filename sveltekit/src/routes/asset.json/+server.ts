e2e67b268496209fd3770514746036cfe823b498
Fernando Maclen <fmaclen@gmail.com>
2022-09-07 11:19:22 -0400

    #34 - Add ability to create a new Asset

---
+++ sveltekit/src/routes/asset.json/+server.ts
@@ -0,0 +1,45 @@
+import { json } from '@sveltejs/kit';
+import type { RequestEvent } from '@sveltejs/kit';
+
+import prisma from '$lib/helpers/prisma';
+
+interface NewAsset {
+	name: string;
+	assetTypeId: number;
+	balanceGroup: number;
+	value: number;
+	symbol?: string;
+	quantity?: number;
+	cost?: number;
+}
+
+export const POST = async ({ request }: RequestEvent) => {
+	const newAsset: NewAsset = await request.json();
+
+	const existingAsset = await prisma.asset.findUnique({ where: { name: newAsset.name } });
+
+	if (existingAsset) {
+		return json({ error: 'An asset with the same name already exists' });
+	}
+
+	const asset = await prisma.asset.create({
+		data: {
+			name: newAsset.name,
+			assetTypeId: newAsset.assetTypeId,
+			symbol: newAsset.symbol,
+			balanceGroup: 1,
+			isSold: false,
+			assetBalanceStatements: {
+				create: [
+					{
+						cost: newAsset.cost,
+						quantity: newAsset.quantity,
+						value: newAsset.value
+					}
+				]
+			}
+		}
+	});
+
+	return json({ id: asset.id });
+};
--- sveltekit/src/routes/asset/add/+page.svelte
+++ sveltekit/src/routes/asset/add/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import Button from '$lib/components/Button.svelte';
-
 	import Form from '$lib/components/Form.svelte';
 	import FormField from '$lib/components/FormField.svelte';
 	import FormFieldset from '$lib/components/FormFieldset.svelte';
@@ -10,6 +9,45 @@
 	import ScrollView from '$lib/components/ScrollView.svelte';
 	import Section from '$lib/components/Section.svelte';
 	import { Appearance } from '$lib/helpers/constants';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	$: name = '';
+	$: chosenAssetType = 1;
+	$: chosenBalanceGroup = 0;
+	$: isQuantifiable = data.quantifiableAssetTypes.includes(chosenAssetType);
+	$: quantity = '';
+	$: cost = '';
+	$: value = isQuantifiable ? `${parseInt(quantity) * parseInt(cost)}` : '';
+	$: error = '';
+	$: isSubmittable =
+		isQuantifiable && name && quantity && cost ? true : name && value ? true : false;
+
+	const handleSubmit = async (event: any) => {
+		const response = await fetch('/asset.json', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				name: event.target.name.value,
+				symbol: event.target.symbol?.value,
+				balanceGroup: chosenBalanceGroup,
+				assetTypeId: chosenAssetType,
+				quantity: parseInt(quantity),
+				cost: parseInt(cost),
+				value: parseInt(value)
+			})
+		});
+		const asset = await response.json();
+
+		if (asset.error) {
+			error = asset.error;
+		} else {
+			alert(`Asset created! (Id: ${asset.id})`);
+		}
+	};
 
 	const title = 'Add asset';
 </script>
@@ -20,32 +58,39 @@
 
 <ScrollView {title}>
 	<Section title="Asset details">
-		<div slot="CONTENT" class="balanceSheet">
-			<Form>
+		<div slot="CONTENT">
+			<Form on:submit={handleSubmit}>
 				<FormFieldset>
 					<FormField name="assetType" label="Asset type">
-						<FormSelect value={1} options={[{ label: 'toto' }, { label: 'goto' }]} />
+						<FormSelect options={data.assetTypes} bind:value={chosenAssetType} />
+					</FormField>
+					<FormField name="balanceGroup" label="Balance group">
+						<FormSelect options={data.balanceGroups} bind:value={chosenBalanceGroup} />
 					</FormField>
 					<FormField name="name" label="Name">
-						<FormInput type="text" name="name" />
+						<FormInput type="text" name="name" bind:value={name} {error} />
 					</FormField>
-					<FormField name="symbol" label="Symbol">
-						<FormInput type="text" name="symbol" />
-					</FormField>
+					{#if isQuantifiable}
+						<FormField name="symbol" label="Symbol" optional={true}>
+							<FormInput type="text" name="symbol" required={false} />
+						</FormField>
+					{/if}
 				</FormFieldset>
 				<FormFieldset>
-					<FormField name="quantity" label="Quantity">
-						<FormInput type="number" name="quantity" />
-					</FormField>
-					<FormField name="cost" label="Cost">
-						<FormInput type="number" name="cost" />
-					</FormField>
+					{#if isQuantifiable}
+						<FormField name="quantity" label="Quantity">
+							<FormInput type="number" name="quantity" bind:value={quantity} />
+						</FormField>
+						<FormField name="cost" label="Cost">
+							<FormInput type="number" name="cost" bind:value={cost} />
+						</FormField>
+					{/if}
 					<FormField name="value" label="Value">
-						<FormInput type="number" name="value" />
+						<FormInput type="number" name="value" disabled={isQuantifiable} bind:value />
 					</FormField>
 				</FormFieldset>
 				<FormFooter>
-					<Button appearance={Appearance.ACTIVE}>Add asset</Button>
+					<Button disabled={!isSubmittable} appearance={Appearance.ACTIVE}>Add asset</Button>
 				</FormFooter>
 			</Form>
 		</div>
