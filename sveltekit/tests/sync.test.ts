4290daf9da1d411309db7d2b4879c5fc058979c3
Fernando Maclen <fmaclen@gmail.com>
2022-10-04 15:02:51 -0400

    add sync test

--- sveltekit/tests/sync.test.ts
+++ sveltekit/tests/sync.test.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import fetch from 'node-fetch';
+import { DeveloperFunctions } from '../src/lib/helpers/constants.js';
 import { expect, test } from '@playwright/test';
 import { databaseWipe, delay } from './fixtures/helpers.js';
 
@@ -12,9 +13,9 @@ test.describe('Sync CanutinFile', () => {
 		await page.goto('/');
 		await page.locator('a', { hasText: 'Settings' }).click();
 		await expect(page.locator('section', { hasText: 'Sync' })).toBeVisible();
-		await expect(
-			page.locator('.formNotice__notice', { hasText: 'Sync is disabled' })
-		).toBeVisible();
+		const formNotice = page.locator('.formNotice__notice');
+		expect(await formNotice.textContent()).toMatch('Sync is disabled');
+		await expect(formNotice).toHaveClass(/formNotice__notice--warning/);
 
 		const urlInput = page.locator('.formInput__input[name=canutinFileUrl]');
 		await expect(urlInput).toHaveValue('');
@@ -25,26 +26,97 @@ test.describe('Sync CanutinFile', () => {
 		await expect(page.locator('a', { hasText: 'Sync' })).not.toBeVisible();
 	});
 
-	test.skip('Fetching a CanutinFile from a url', async ({ page, baseURL }) => {
+	test('Fetching a CanutinFile from a url', async ({ page, baseURL }) => {
+		// Check the vault is empty
 		await page.goto('/');
+		await expect(page.locator('h1', { hasText: 'The big picture' })).toBeVisible();
+		expect(await page.locator('.card', { hasText: 'Net worth' }).textContent()).toMatch('$0');
+
+		// Check there is no Sync button in the sidebar
+		const sidebarSyncButton = page.locator('button', { hasText: 'Sync' });
+		await expect(sidebarSyncButton).not.toBeVisible();
+
 		await page.locator('a', { hasText: 'Settings' }).click();
 		await expect(page.locator('section', { hasText: 'Sync' })).toBeVisible();
+		await expect(sidebarSyncButton).not.toBeVisible();
 
-		// client: check sync button on sidebar **is invisible**
-		// client: refresh page and check sync button on sidebar **is still invisible**
-		// client: set url (perhaps as /devTools.json)
-		// client: set cookie
-		// client: set jwt
-		// server: check the received request is correct
-		// client: check sync is enabled
-		// client: check sync button on sidebar is enabled
-		// client: refresh page and check that sync is still enabled
-		// client: click sync button
-		// server: check the received request is correct
-		// client: check a success message is shown in the status bar
-		// client: set an incorrect url
-		// client: check sync button on sidebar **is visible** and is disabled // NEED TO IMPLEMENT THIS FIRST
-		// client: check status bar shows a warning message
-		// client: refresh page and check that sync **is still visible** and is disabled
+		const urlInput = page.locator('.formInput__input[name=canutinFileUrl]');
+		const cookieInput = page.locator('.formInput__input[name=cookie]');
+		const jwtInput = page.locator('.formInput__input[name=jwt]');
+		const dismissButton = page.locator('.button', { hasText: 'Dismiss' });
+		let submitButton = page.locator('button', { hasText: 'Enable' });
+
+		// This endpoint will return `tests/fixtures/canutinFile-maximum-data.json` as a response
+		await urlInput.fill(
+			`${baseURL}/devTools.json?functionType=${DeveloperFunctions.CANUTIN_FILE_SYNC_TEST}`
+		);
+		await expect(submitButton).not.toBeDisabled();
+
+		// Submit form with only a URL when the server expects a cookie and JWT
+		await submitButton.click();
+		expect(await page.locator('.statusBar--warning').textContent()).toMatch(
+			"Coudn't fetch a CanutinFile JSON from the provided URL"
+		);
+
+		// Submit form with only a cookie
+		await dismissButton.click();
+		await cookieInput.fill('accessToken=1234abc; userId=1234; Path=/; HttpOnly;');
+		await submitButton.click();
+		expect(await page.locator('.statusBar--warning').textContent()).toMatch(
+			"Coudn't fetch a CanutinFile JSON from the provided URL"
+		);
+
+		// Submit form with a JWT
+		await dismissButton.click();
+		await jwtInput.fill(
+			'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+		);
+		await submitButton.click();
+		expect(await page.locator('.statusBar--positive').textContent()).toMatch(
+			'Sync was enabled succesfully'
+		);
+
+		// Check the notice has changed
+		const formNotice = page.locator('.formNotice__notice');
+		expect(await formNotice.textContent()).toMatch('Sync is enabled');
+		await expect(formNotice).toHaveClass(/formNotice__notice--positive/);
+
+		// Check the label of the submit form button has changed
+		submitButton = page.locator('button', { hasText: 'Update' });
+		await expect(submitButton).not.toBeDisabled();
+
+		// Clear the url field to disable the submit button
+		await urlInput.fill('');
+		await expect(submitButton).toBeDisabled();
+
+		// Check the sidebar has a Sync button
+		await expect(sidebarSyncButton).toBeVisible();
+
+		await page.reload();
+		await expect(sidebarSyncButton).toBeVisible();
+
+		// Trigger a sync
+		await sidebarSyncButton.click();
+		await expect(sidebarSyncButton).toBeDisabled();
+		expect(await page.locator('.statusBar--active').textContent()).toMatch('Syncing...');
+		expect(await page.locator('.statusBar--positive').textContent()).toMatch(
+			'Sync updated 2 accounts and 2 assets'
+		);
+		await expect(sidebarSyncButton).not.toBeDisabled();
+
+		// Check the server response was imported correctly
+		await page.locator('a', { hasText: 'The big picture' }).click();
+		await expect(page.locator('h1', { hasText: 'The big picture' })).toBeVisible();
+		expect(await page.locator('.card', { hasText: 'Net worth' }).textContent()).toMatch('$80,340');
+
+		// Disable sync by entering server information that doesn't return a response
+		await page.locator('a', { hasText: 'Settings' }).click();
+		await urlInput.fill(`${baseURL}/nonExistentEndpoint.json`);
+		await submitButton.click();
+
+		await expect(formNotice).toHaveClass(/formNotice__notice--warning/);
+		expect(await formNotice.textContent()).toMatch('Sync is disabled');
+		await expect(sidebarSyncButton).toBeVisible();
+		await expect(sidebarSyncButton).toBeDisabled();
 	});
 });
