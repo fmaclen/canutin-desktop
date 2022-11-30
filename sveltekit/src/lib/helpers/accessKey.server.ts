a250e548729061eb0f683db9c53d0c632bd9cd78
Fernando Maclen <fmaclen@gmail.com>
2022-11-30 10:46:27 -0500

    fix: cleanup redundant syntax

--- sveltekit/src/lib/helpers/accessKey.server.ts
+++ sveltekit/src/lib/helpers/accessKey.server.ts
@@ -2,26 +2,19 @@ import prisma from '$lib/helpers/prisma';
 import { AccessKeySettings, ACCESS_KEY_COOKIE_NAME } from '$lib/helpers/constants';
 import type { AccessKeyStatusStore } from '$lib/stores/accessKeyStatusStore';
 
-export const getAccessKeySettings = async (): Promise<AccessKeyStatusStore> => {
-	const accessKeySettings = Object.values(AccessKeySettings);
-	const accessKeyValues = await prisma.setting.findMany({
-		where: { name: { in: accessKeySettings } }
-	});
-
-	const accessKey = accessKeyValues.find(
-		(setting) => setting.name === AccessKeySettings.ACCESS_KEY
-	);
-
-	return {
-		accessKey: accessKey?.value
-	};
-};
-
 export const getVaultAccessKey = async () => {
 	return await prisma.setting.findUnique({
 		where: { name: AccessKeySettings.ACCESS_KEY },
 		select: { value: true }
 	});
+};
+
+export const getAccessKeySettings = async (): Promise<AccessKeyStatusStore> => {
+	const accessKey = await getVaultAccessKey();
+
+	return {
+		accessKey: accessKey ? accessKey?.value : undefined
+	};
 };
 
 export const isRequestAuthorized = async (request: Request) => {
