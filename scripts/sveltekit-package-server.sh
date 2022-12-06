92d49795931845228f531637e623f626d63af11b
Fernando Maclen <fmaclen@gmail.com>
2022-12-06 12:00:12 -0500

    fix: typo

--- scripts/sveltekit-package-server.sh
+++ scripts/sveltekit-package-server.sh
@@ -3,7 +3,7 @@
 # This script packages SvelteKit for deployment on a Linux server
 # NOTE: this script needs to run after `sveltekit-package-electron.js`
 
-# Move packaged SvelteKit to /linux-server/canutin (without override existing files in destination folder)
+# Move packaged SvelteKit to /linux-server/canutin (without overriding existing files in destination folder)
 mv -n ./resources/sveltekit/* ./linux-server/canutin
 
 # Create a tar file of the /linux-server/canutin folder files
