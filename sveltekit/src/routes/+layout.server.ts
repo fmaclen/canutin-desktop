224f2c4ddf8cd2bc405d0341510f624451a6c0a1
Fernando Maclen <fmaclen@gmail.com>
2022-12-19 14:22:17 -0300

    fix: app version is undefined
    Fixes #156

--- sveltekit/src/routes/+layout.server.ts
+++ sveltekit/src/routes/+layout.server.ts
@@ -1,10 +1,8 @@
-import { env } from '$env/dynamic/private';
-
 import { getSyncStatus } from '$lib/helpers/import.server';
 
 export const load = async () => {
 	return {
-		appVersion: env.APP_VERSION,
+		appVersion: process.env.APP_VERSION,
 		syncStatus: await getSyncStatus()
 	};
 };
