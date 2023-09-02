b9c61d83474097f2b262ce7ca71fa5ec745bd6f4
Fernando Maclen <fmaclen@gmail.com>
2023-09-02 10:31:00 -0400

    wip: hacky implementation of performance table

--- sveltekit/src/routes/trends/+page.server.ts
+++ sveltekit/src/routes/trends/+page.server.ts
@@ -3,10 +3,20 @@ import prisma from '$lib/helpers/prisma.server';
 import type { ChartDataset } from 'chart.js';
 
 import { getAccountCurrentBalance, getAssetCurrentBalance } from '$lib/helpers/models.server';
-import { sub, eachWeekOfInterval, endOfWeek } from 'date-fns';
+import {
+	eachWeekOfInterval,
+	endOfWeek,
+	isSameWeek,
+	startOfWeek,
+	startOfYear,
+	subMonths,
+	subWeeks,
+	subYears
+} from 'date-fns';
 import { BalanceGroup, SortOrder } from '$lib/helpers/constants';
 import { setChartDatasetColor } from '$lib/helpers/charts';
 import { handlePeriodEnd } from '$lib/helpers/charts';
+import { percentageOf, proportionBetween } from '$lib/helpers/misc';
 
 export const load = async () => {
 	const labels: string[] = [];
@@ -16,7 +26,53 @@ export const load = async () => {
 	const datasetInvestments: ChartDataset[] = [];
 	const datasetOtherAssets: ChartDataset[] = [];
 
-	// Add balance to dataset
+	interface TableNetWorth {
+		name: BalanceGroup | 'Net worth';
+		currentBalance: number;
+		balanceOneWeek: number;
+		performanceOneWeek: number;
+		balanceOneMonth: number;
+		performanceOneMonth: number;
+		balanceSixMonths: number;
+		performanceSixMonths: number;
+		balanceYearToDate: number;
+		performanceYearToDate: number;
+		balanceOneYear: number;
+		performanceOneYear: number;
+		balanceFiveYears: number;
+		performanceFiveYears: number;
+		balanceLifetime: number;
+		performanceLifetime: number;
+		allocation: number;
+	}
+
+	const performanceZeroes = {
+		currentBalance: 0,
+		balanceOneWeek: 0,
+		balanceOneMonth: 0,
+		balanceSixMonths: 0,
+		balanceYearToDate: 0,
+		balanceOneYear: 0,
+		balanceFiveYears: 0,
+		balanceLifetime: 0,
+		performanceOneWeek: 0,
+		performanceOneMonth: 0,
+		performanceSixMonths: 0,
+		performanceYearToDate: 0,
+		performanceOneYear: 0,
+		performanceFiveYears: 0,
+		performanceLifetime: 0,
+		allocation: 0
+	};
+
+	const tableNetWorth: TableNetWorth[] = [
+		{ name: 'Net worth', ...performanceZeroes },
+		{ name: BalanceGroup.CASH, ...performanceZeroes },
+		{ name: BalanceGroup.DEBT, ...performanceZeroes },
+		{ name: BalanceGroup.INVESTMENTS, ...performanceZeroes },
+		{ name: BalanceGroup.OTHER_ASSETS, ...performanceZeroes }
+	];
+
 	const balanceToDataset = (label: string, balance: number, balanceGroup?: BalanceGroup) => {
 		const itemData = { label, data: [balance] };
 
@@ -44,14 +100,56 @@ export const load = async () => {
 		}
 	};
 
-	const endOfThisWeek = endOfWeek(new Date());
+	const accounts = await prisma.account.findMany();
+	const assets = await prisma.asset.findMany();
+
+	//////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////
+
+	const earliestBalanceDates: Date[] = [];
+	for (const account of accounts) {
+		let earliestBalance;
+		if (account.isAutoCalculated) {
+			earliestBalance = await prisma.transaction.findFirst({
+				where: { accountId: account.id },
+				orderBy: { date: SortOrder.ASC }
+			});
+		} else {
+			earliestBalance = await prisma.accountBalanceStatement.findFirst({
+				where: { accountId: account.id },
+				orderBy: { createdAt: SortOrder.ASC }
+			});
+		}
+		if (earliestBalance) earliestBalanceDates.push(earliestBalance.createdAt);
+	}
+
+	for (const asset of assets) {
+		const earliestBalance = await prisma.assetBalanceStatement.findFirst({
+			where: { assetId: asset.id },
+			orderBy: { createdAt: SortOrder.ASC }
+		});
+		if (earliestBalance) earliestBalanceDates.push(earliestBalance.createdAt);
+	}
+
+	earliestBalanceDates.sort((a, b) => (a > b ? 1 : -1));
+
+	const today = new Date();
+	const endOfThisWeek = endOfWeek(today);
+	const oneWeekAgo = subWeeks(today, 1);
+	const oneMonthAgo = subMonths(today, 1);
+	const sixMonthsAgo = subMonths(today, 6);
+	const firstOfCurrentYear = startOfYear(today);
+	const oneYearAgo = subYears(today, 1);
+	const fiveYearsAgo = subYears(today, 5);
 	const weeksInPeriod = eachWeekOfInterval({
-		start: sub(endOfThisWeek, { years: 2 }), // Two years ago
+		start: startOfWeek(earliestBalanceDates[0]),
 		end: handlePeriodEnd(endOfThisWeek)
 	});
 
-	const accounts = await prisma.account.findMany();
-	const assets = await prisma.asset.findMany();
+	//////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////
 
 	for (const weekInPeriod of weeksInPeriod) {
 		labels.push(weekInPeriod.toISOString().slice(0, 10)); // e.g. 2022-12-31
@@ -99,7 +197,109 @@ export const load = async () => {
 		balanceToDataset('Debt', debtPeriod);
 		balanceToDataset('Investments', investmentsPeriod);
 		balanceToDataset('Other assets', otherAssetsPeriod);
+
+		////////////////////////////////////////////////////////////////////////////
+		////////////////////////////////////////////////////////////////////////////
+		////////////////////////////////////////////////////////////////////////////
+
+		if (isSameWeek(weekInPeriod, weeksInPeriod[0])) {
+			(tableNetWorth.find(({ name }) => name === 'Net worth') as TableNetWorth).balanceLifetime = netWorthPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.CASH) as TableNetWorth).balanceLifetime = cashPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.DEBT) as TableNetWorth).balanceLifetime = debtPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.INVESTMENTS) as TableNetWorth).balanceLifetime = investmentsPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.OTHER_ASSETS) as TableNetWorth).balanceLifetime = otherAssetsPeriod; // prettier-ignore
+		}
+		if (isSameWeek(weekInPeriod, oneWeekAgo)) {
+			(tableNetWorth.find(({ name }) => name === 'Net worth') as TableNetWorth).balanceOneWeek = netWorthPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.CASH) as TableNetWorth).balanceOneWeek = cashPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.DEBT) as TableNetWorth).balanceOneWeek = debtPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.INVESTMENTS) as TableNetWorth).balanceOneWeek = investmentsPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.OTHER_ASSETS) as TableNetWorth).balanceOneWeek = otherAssetsPeriod; // prettier-ignore
+		}
+		if (isSameWeek(weekInPeriod, oneMonthAgo)) {
+			(tableNetWorth.find(({ name }) => name === 'Net worth') as TableNetWorth).balanceOneMonth = netWorthPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.CASH) as TableNetWorth).balanceOneMonth = cashPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.DEBT) as TableNetWorth).balanceOneMonth = debtPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.INVESTMENTS) as TableNetWorth).balanceOneMonth = investmentsPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.OTHER_ASSETS) as TableNetWorth).balanceOneMonth = otherAssetsPeriod; // prettier-ignore
+		}
+		if (isSameWeek(weekInPeriod, sixMonthsAgo)) {
+			(tableNetWorth.find(({ name }) => name === 'Net worth') as TableNetWorth).balanceSixMonths = netWorthPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.CASH) as TableNetWorth).balanceSixMonths = cashPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.DEBT) as TableNetWorth).balanceSixMonths = debtPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.INVESTMENTS) as TableNetWorth).balanceSixMonths = investmentsPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.OTHER_ASSETS) as TableNetWorth).balanceSixMonths = otherAssetsPeriod; // prettier-ignore
+		}
+		if (isSameWeek(weekInPeriod, firstOfCurrentYear)) {
+			(tableNetWorth.find(({ name }) => name === 'Net worth') as TableNetWorth).balanceYearToDate = netWorthPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.CASH) as TableNetWorth).balanceYearToDate = cashPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.DEBT) as TableNetWorth).balanceYearToDate = debtPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.INVESTMENTS) as TableNetWorth).balanceYearToDate = investmentsPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.OTHER_ASSETS) as TableNetWorth).balanceYearToDate = otherAssetsPeriod; // prettier-ignore
+		}
+		if (isSameWeek(weekInPeriod, oneYearAgo)) {
+			(tableNetWorth.find(({ name }) => name === 'Net worth') as TableNetWorth).balanceOneYear = netWorthPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.CASH) as TableNetWorth).balanceOneYear = cashPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.DEBT) as TableNetWorth).balanceOneYear = debtPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.INVESTMENTS) as TableNetWorth).balanceOneYear = investmentsPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.OTHER_ASSETS) as TableNetWorth).balanceOneYear = otherAssetsPeriod; // prettier-ignore
+		}
+		if (isSameWeek(weekInPeriod, fiveYearsAgo)) {
+			(tableNetWorth.find(({ name }) => name === 'Net worth') as TableNetWorth).balanceFiveYears = netWorthPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.CASH) as TableNetWorth).balanceFiveYears = cashPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.DEBT) as TableNetWorth).balanceFiveYears = debtPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.INVESTMENTS) as TableNetWorth).balanceFiveYears = investmentsPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.OTHER_ASSETS) as TableNetWorth).balanceFiveYears = otherAssetsPeriod; // prettier-ignore
+		}
+		if (weekInPeriod === weeksInPeriod[weeksInPeriod.length - 1]) {
+			(tableNetWorth.find(({ name }) => name === 'Net worth') as TableNetWorth).currentBalance = netWorthPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === 'Net worth') as TableNetWorth).allocation = 100; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.CASH) as TableNetWorth).currentBalance = cashPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.CASH) as TableNetWorth).allocation = proportionBetween(cashPeriod, netWorthPeriod); // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.DEBT) as TableNetWorth).currentBalance = debtPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.DEBT) as TableNetWorth).allocation = proportionBetween(Math.abs(debtPeriod), netWorthPeriod); // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.INVESTMENTS) as TableNetWorth).currentBalance = investmentsPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.INVESTMENTS) as TableNetWorth).allocation = proportionBetween(investmentsPeriod, netWorthPeriod); // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.OTHER_ASSETS) as TableNetWorth).currentBalance = otherAssetsPeriod; // prettier-ignore
+			(tableNetWorth.find(({ name }) => name === BalanceGroup.OTHER_ASSETS) as TableNetWorth).allocation = proportionBetween(otherAssetsPeriod, netWorthPeriod); // prettier-ignore
+		}
 	}
+
+	for (const performance in tableNetWorth) {
+		const performanceItem = tableNetWorth[performance];
+		performanceItem.performanceOneWeek = percentageOf(
+			performanceItem.balanceOneWeek,
+			performanceItem.currentBalance
+		);
+		performanceItem.performanceOneMonth = percentageOf(
+			performanceItem.balanceOneMonth,
+			performanceItem.currentBalance
+		);
+		performanceItem.performanceSixMonths = percentageOf(
+			performanceItem.balanceSixMonths,
+			performanceItem.currentBalance
+		);
+		performanceItem.performanceYearToDate = percentageOf(
+			performanceItem.balanceYearToDate,
+			performanceItem.currentBalance
+		);
+		performanceItem.performanceOneYear = percentageOf(
+			performanceItem.balanceOneYear,
+			performanceItem.currentBalance
+		);
+		performanceItem.performanceFiveYears = percentageOf(
+			performanceItem.balanceFiveYears,
+			performanceItem.currentBalance
+		);
+		performanceItem.performanceLifetime = percentageOf(
+			performanceItem.balanceLifetime,
+			performanceItem.currentBalance
+		);
+	}
+
+	////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
 
 	// Generate shades of a given color for each dataset
 	const COLOR_WEIGHT = 125;
@@ -156,6 +356,7 @@ export const load = async () => {
 		datasetCash,
 		datasetDebt,
 		datasetInvestments,
-		datasetOtherAssets
+		datasetOtherAssets,
+		tableNetWorth
 	};
 };
--- sveltekit/src/routes/trends/+page.svelte
+++ sveltekit/src/routes/trends/+page.svelte
@@ -3,11 +3,42 @@
 	import ScrollView from '$lib/components/ScrollView.svelte';
 	import Section from '$lib/components/Section.svelte';
 	import ChartJs from '$lib/components/ChartJS.svelte';
+	import Table from '$lib/components/Table.svelte';
+	import TableTh from '$lib/components/TableTh.svelte';
+	import TableTr from '$lib/components/TableTr.svelte';
+	import TableTd from '$lib/components/TableTd.svelte';
+	import TableButtonSortable from '$lib/components/TableButtonSortable.svelte';
+	import TableNoValue from '$lib/components/TableNoValue.svelte';
 	import type { PageData } from './$types';
+	import { formatCurrency, toCamelCase } from '$lib/helpers/misc';
+	import { SortOrder, getBalanceGroupLabel } from '$lib/helpers/constants';
 
 	const title = 'Trends';
 
 	export let data: PageData;
+
+	enum TableHeaders {
+		BALANCE_TYPE = 'Balance type',
+		ONE_WEEK = '1 week',
+		ONE_MONTH = '1 month',
+		SIX_MONTHS = '6 months',
+		YEAR_TO_DATE = 'Year to date',
+		ONE_YEAR = '1 year',
+		FIVE_YEARS = '5 years',
+		LIFETIME = 'Lifetime',
+		ALLOCATION = 'Allocation'
+	}
+
+	// Default params
+	let sortOrder = SortOrder.DESC;
+	let sortBy: string;
+
+	let tableHeaders = Object.values(TableHeaders).map((tableHeader) => {
+		return {
+			label: tableHeader,
+			column: toCamelCase(tableHeader)
+		};
+	});
 </script>
 
 <Head {title} />
@@ -16,6 +47,79 @@
 	<Section title="Net worth">
 		<div slot="CONTENT">
 			<ChartJs labels={data.labels} datasets={data.datasetNetWorth} />
+			<Table>
+				<thead>
+					<tr>
+						{#each tableHeaders as tableHeader}
+							{@const { label, column } = tableHeader}
+							<TableTh
+								isAlignedRight={[
+									TableHeaders.ONE_WEEK,
+									TableHeaders.ONE_MONTH,
+									TableHeaders.SIX_MONTHS,
+									TableHeaders.YEAR_TO_DATE,
+									TableHeaders.ONE_YEAR,
+									TableHeaders.FIVE_YEARS,
+									TableHeaders.LIFETIME,
+									TableHeaders.ALLOCATION
+								].includes(tableHeader.label)}
+							>
+								<TableButtonSortable {label} {sortOrder} sortBy={sortBy === column} />
+							</TableTh>
+						{/each}
+					</tr>
+				</thead>
+				<tbody>
+					{#each data.tableNetWorth as balanceType}
+						<TableTr>
+							<TableTd>
+								{typeof balanceType.name === 'string'
+									? balanceType.name
+									: getBalanceGroupLabel(balanceType.name)}
+							</TableTd>
+							<TableTd hasTotal={true} isAlignedRight={true}>
+								<u title={formatCurrency(balanceType.balanceOneWeek, 2, 2)}>
+									{balanceType.performanceOneWeek}%
+								</u>
+							</TableTd>
+							<TableTd hasTotal={true} isAlignedRight={true}>
+								<u title={formatCurrency(balanceType.balanceOneMonth, 2, 2)}>
+									{balanceType.performanceOneMonth}%
+								</u>
+							</TableTd>
+							<TableTd hasTotal={true} isAlignedRight={true}>
+								<u title={formatCurrency(balanceType.balanceSixMonths, 2, 2)}>
+									{balanceType.performanceSixMonths}%
+								</u>
+							</TableTd>
+							<TableTd hasTotal={true} isAlignedRight={true}>
+								<u title={formatCurrency(balanceType.balanceYearToDate, 2, 2)}>
+									{balanceType.performanceYearToDate}%
+								</u>
+							</TableTd>
+							<TableTd hasTotal={true} isAlignedRight={true}>
+								<u title={formatCurrency(balanceType.balanceOneYear, 2, 2)}>
+									{balanceType.performanceOneYear}%
+								</u>
+							</TableTd>
+							<TableTd hasTotal={true} isAlignedRight={true}>
+								<u title={formatCurrency(balanceType.balanceFiveYears, 2, 2)}>
+									{balanceType.performanceFiveYears}%
+								</u>
+							</TableTd>
+							<TableTd hasTotal={true} isAlignedRight={true}>
+								<u title={formatCurrency(balanceType.balanceLifetime, 2, 2)}>
+									{balanceType.performanceLifetime}%
+								</u>
+							</TableTd>
+							<TableTd hasTotal={true} isAlignedRight={true}>
+								{formatCurrency(balanceType.currentBalance, 2, 2)}
+								({balanceType.allocation}%)
+							</TableTd>
+						</TableTr>
+					{/each}
+				</tbody>
+			</Table>
 		</div>
 	</Section>
 
