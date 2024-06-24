836efca8ad36d77f8bc25435f956d7e097db90b7
Fernando Maclen <fmaclen@gmail.com>
2024-06-24 11:46:03 -0400

    remove "Open browser" option in TrayMenu

--- electron/tray-menu.ts
+++ electron/tray-menu.ts
@@ -3,7 +3,6 @@ import {
   app,
   Menu,
   MenuItemConstructorOptions,
-  shell,
   Tray,
   nativeTheme,
   BrowserWindow,
@@ -13,7 +12,7 @@ import Vault from "./vault";
 import Server from "./server";
 
 class TrayMenu {
-  static readonly OPEN_BROWSER_DELAY = 500;
+  static readonly LOAD_SERVER_URL_DELAY = 500;
 
   static readonly MENU_SERVER_STATUS = "menu-server-status";
   static readonly MENU_SERVER_TOGGLE = "menu-server-toggle";
@@ -38,7 +37,6 @@ class TrayMenu {
 
   private menuServerStatus: MenuItemConstructorOptions;
   private menuServerToggle: MenuItemConstructorOptions;
-  private menuOpenInBrowser: MenuItemConstructorOptions;
   private menuVaultPath: MenuItemConstructorOptions;
   private menuSwitchVault: MenuItemConstructorOptions;
   private menuPresistentOptions: MenuItemConstructorOptions[];
@@ -74,20 +72,6 @@ class TrayMenu {
       id: TrayMenu.MENU_SERVER_TOGGLE,
       visible: false,
     };
-    this.menuOpenInBrowser = {
-      label: "Open in browser",
-      id: TrayMenu.MENU_OPEN_IN_BROWSER,
-      accelerator: this.isMacOs ? "Command+T" : "Ctrl+T",
-      visible: false,
-      click: () => this.openBrowser(),
-    };
-    this.menuOpenInBrowser = {
-      label: "Open in browser",
-      id: TrayMenu.MENU_OPEN_IN_BROWSER,
-      accelerator: this.isMacOs ? "Command+T" : "Ctrl+T",
-      visible: false,
-      click: () => this.openBrowser(),
-    };
     this.menuVaultPath = {
       label: this.vault?.path ? this.vault.path : "No vault chosen",
       id: TrayMenu.MENU_VAULT_PATH,
@@ -115,7 +99,6 @@ class TrayMenu {
       this.menuServerStatus,
       this.menuSeparator,
       this.menuServerToggle,
-      this.menuOpenInBrowser,
       this.menuSeparator,
       this.menuVaultPath,
       this.menuSwitchVault,
@@ -137,7 +120,6 @@ class TrayMenu {
     // Start the server
     if (this.vault?.path) {
       this.toggleServer();
-      this.isAppPackaged && this.openBrowser(TrayMenu.OPEN_BROWSER_DELAY);
     }
   }
 
@@ -155,7 +137,6 @@ class TrayMenu {
         this.toggleServer(); // Starts the server
       } else {
         this.toggleServer(); // Starts the server
-        this.openBrowser(TrayMenu.OPEN_BROWSER_DELAY);
       }
     }
   };
@@ -173,7 +154,6 @@ class TrayMenu {
       this.menuServerStatus.icon = this.getImagePath(
         TrayMenu.ICON_STATUS_NEGATIVE
       );
-      this.menuOpenInBrowser.visible = false;
       this.setTrayIcon(TrayMenu.ICON_TRAY_IDLE);
       this.updateTray();
       this.setLoadingView();
@@ -186,10 +166,9 @@ class TrayMenu {
       this.menuServerStatus.icon = this.getImagePath(
         TrayMenu.ICON_STATUS_POSITIVE
       );
-      this.menuOpenInBrowser.visible = true;
       this.setTrayIcon(TrayMenu.ICON_TRAY_ACTIVE);
       this.updateTray();
-      setTimeout(() => this.server && this.window.loadURL(this.server.url), TrayMenu.OPEN_BROWSER_DELAY);
+      setTimeout(() => this.server && this.window.loadURL(this.server.url), TrayMenu.LOAD_SERVER_URL_DELAY);
       ;
     }
 
@@ -203,15 +182,6 @@ class TrayMenu {
   private updateTray = () => {
     this.tray?.setContextMenu(Menu.buildFromTemplate(this.menuCurrentTemplate));
   };
-
-  private openBrowser(delay: number = 0) {
-    const server = this.server;
-    // FIXME:
-    // To prevent opening the browser before the server is ready we wait.
-    // Server boot up time is likely to vary so ideally we would "ping"
-    // the server until it's ready and only then open the user's browser.
-    server?.url && setTimeout(() => shell.openExternal(server.url), delay);
-  }
 
   private getImagePath(fileName: string) {
     const themeAgnosticIcons = [
